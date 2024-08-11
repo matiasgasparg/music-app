3c5823a6716b958a43769b6c9da1d9a94328eb8c
mateoS <texplorador135@gmail.com>
2024-08-11 20:50:09 -0300

    Corrigiendo el Link

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { Link } from './Link';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
-  return React.createElement(
-    'div',
-    null,
-    React.createElement('h1', null, '404 - This is NOT fine'),
-    React.createElement('img', {
-      src: 'https://midu.dev/images/this-is-fine-404.gif',
-      alt: 'This is NOT Fine Matias'
-    }),
-    React.createElement(Link, { to: '/' }, 'Volver al inicio')
+  return (
+    <div style={{ 
+      textAlign: 'end', 
+      backgroundColor: 'black', 
+      height: '150vh', 
+      display: 'flex', 
+      flexDirection: 'column', 
+      justifyContent: 'top', 
+      alignItems: 'center' 
+      }}>
+      <h1 style={{color: 'white'}}>404 - This is NOT fine</h1>
+      <img src='https://midu.dev/images/this-is-fine-404.gif' alt='This is NOT Fine Matias' />
+      <Link to="/songs" style={{ 
+        color: 'orange', 
+        marginTop: '20px' 
+        }}>
+        Volver al inicio
+      </Link>
+    </div>
   );
 };
 
